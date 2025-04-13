11a8a6460f08fdd6c5ef488bba53276e13df8ce5
Gscienty <gaoxiaochuan@hotmail.com>
2025-04-13 22:23:52 +0800

    loop play

--- player/src/lib.rs
+++ player/src/lib.rs
@@ -1,3 +1,4 @@
+use bili_api::AudioInfo;
 use player::AudioPlayer;
 
 mod bili_api;
@@ -10,17 +11,31 @@ mod player;
 mod progress;
 
 #[wasm_bindgen_futures::wasm_bindgen::prelude::wasm_bindgen]
-pub async fn run(bvid: &str) {
+pub async fn run(bvid_json: &str) {
+    let bvid_list: Vec<String> = serde_json::from_str(bvid_json).unwrap();
+    loop {
+        for bvid in bvid_list.iter() {
+            let Some(audio) = get_audio(bvid).await else {
+                continue;
+            };
+
+            let mut player = AudioPlayer::new().await.unwrap();
+            player.set_audio(&audio).await.unwrap();
+
+            player.start().await;
+        }
+    }
+}
+
+async fn get_audio(bvid: &str) -> Option<AudioInfo> {
     let audio_info = bili_api::audio_info(bvid).await.unwrap();
-    let audio = &audio_info.audios[1];
+    for info in audio_info.audios.iter() {
+        if info.codecs == "mp4a.40.2" {
+            return Some(info.clone());
+        }
+    }
 
-    let mut player = AudioPlayer::new().await.unwrap();
-    player.set_audio(audio).await.unwrap();
-    player.set_loop(true);
-
-    player.start().await;
-
-    crate::loginfo("done".to_string());
+    None
 }
 
 pub fn loginfo(log: String) {
